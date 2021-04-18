659fb00773d6a8420149c0c53bd16ff4293d8b1b
Ashish <ashish.patel9981@gmail.com>
2021-04-19 03:10:23 +0530

    Adding Repository to communicate with data base and added operation Create , Read , Write  and Delete

---
+++ BookStore-API/Contracts/IRepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using BookStore_API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<IList<T>> FindAll();
+
+        Task<T> FindById(int id);
+
+        Task<bool> Update(Author entity);
+        Task<bool> Save();
+        Task<bool> Delete(Author entity);
+        Task<bool> Create(Author entity);
+
+    }
+}
