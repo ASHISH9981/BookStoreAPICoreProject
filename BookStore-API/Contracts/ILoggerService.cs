229055b03c767e4955196ee6a21886f0a5bceb6b
Ashish <ashish.patel9981@gmail.com>
2021-04-19 00:14:16 +0530

    Add project files.

---
+++ BookStore-API/Contracts/ILoggerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface ILoggerService
+    {
+        void logInfo(string message);
+        void logWarning(string message);
+        void logError(string message);
+        void logDebug(string message);
+    }
+}
---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _loggerService;
+
+        public HomeController( ILoggerService loggerService)
+        {
+            _loggerService = loggerService;
+        }
+        /// <summary>
+        /// Get API Home Page
+       /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _loggerService.logInfo("GET METHOD HOME CONTROLLER");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _loggerService.logDebug("Post METHOD HOME CONTROLLER");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+           _loggerService.logError("DELETE METHOD HOME CONTROLLER");
+        }
+    }
+}
